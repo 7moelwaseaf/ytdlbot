31b6d1f1e237a4c5bfb6e716a5270c1cd6d97ec5
7moelwaseaf <121396301+7moelwaseaf@users.noreply.github.com>
2023-01-01 16:11:07 +0200

    Update config.py

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -31,7 +31,7 @@ MULTIPLY = os.getenv("MULTIPLY", 10)  # VIP1 is 5*5-25G, VIP2 is 50G
 USD2CNY = os.getenv("USD2CNY", 6)  # $5 --> ¥30
 
 ENABLE_VIP = os.getenv("VIP", False)
-MAX_DURATION = int(os.getenv("MAX_DURATION", 60))
+MAX_DURATION = int(os.getenv("MAX_DURATION", 160))
 AFD_LINK = os.getenv("AFD_LINK", "https://afdian.net/@BennyThink")
 COFFEE_LINK = os.getenv("COFFEE_LINK", "https://www.buymeacoffee.com/bennythink")
 COFFEE_TOKEN = os.getenv("COFFEE_TOKEN")
