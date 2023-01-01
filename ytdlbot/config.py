2f287ae07f660fc4ea7f65f7c42318d1fe5dd0a4
7moelwaseaf <121396301+7moelwaseaf@users.noreply.github.com>
2023-01-01 17:05:48 +0200

    Update config.py

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -58,5 +58,5 @@ ARCHIVE_ID = os.getenv("ARCHIVE_ID")
 
 IPv6 = os.getenv("IPv6", False)
 ENABLE_FFMPEG = os.getenv("ENABLE_FFMPEG", False)
-RATE = float(os.getenv("RATE", 1 * 1))
-BURST = int(os.getenv("BURST", 1))
+RATE = float(os.getenv("RATE", 1 * 0.1))
+BURST = int(os.getenv("BURST", 10))
